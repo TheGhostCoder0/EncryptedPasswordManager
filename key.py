c7a2f6ca74de3aabd2a3a84b4846e816c2e4916c
TheGhostCoder0 <105943310+TheGhostCoder0@users.noreply.github.com>
2023-06-05 03:05:31 -0700

    Updated key.py by adding comments

--- key.py
+++ key.py
@@ -1,5 +1,12 @@
+# This script is used to generate a key for the encryption and decryption
+# of passwords in the password manager.
+
 from cryptography.fernet import Fernet
 
+# Generate a new key using the Fernet method from the cryptography library.
 key = Fernet.generate_key()
+
+# Open the key file in write mode and save the generated key. 
+# If the key file does not exist, it will be created.
 with open('key.key', 'wb') as key_file:
     key_file.write(key)
